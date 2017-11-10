36d36fccd4e19b0a3277cefe1ee4e94620635180
Mr.cz <hkdnxycz@outlook.com>
2017-11-10 17:16:53 +0800

    python
    Signed-off-by: Arliki <hkdnxycz@outlook.com>

---
+++ test1/my1.py
@@ -0,0 +1,5 @@
+from myfunction import ComRe
+while 1:
+    a=input("input num:\n")
+    b=ComRe.input_type(a)
+    print(b)
---
+++ test1/myfunction.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2017/11/6 0006 11:03
+# @Author  : Arliki
+# @Contact : hdknxycz@outlook.com 
+# @Site    : 
+# @File    : my2.py
+# @Software: PyCharm
+import re
+class ComRe(object):
+    '''判断输入的类型'''
+    def input_type(a):
+        atype=re.search('[a-zA-Z]+?',a)
+        if atype:
+            return str
+        else:
+            atype=re.search('^(\-|\+)?\d+\.\d+$',a)
+            if atype:
+                return float
+            else:
+                atype=re.search('\W+',a)
+                if atype:
+                    return str
+                else:
+                    return int
