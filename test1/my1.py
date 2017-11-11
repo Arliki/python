669c03d2059789465bb3578ee31783fcc66d5ce6
Arliki <hkdnxycz@outlook.com>
2017-11-11 20:01:59 +0800

    python test

--- test1/my1.py
+++ test1/my1.py
@@ -1,5 +1,20 @@
 from myfunction import ComRe
-while 1:
-    a=input("input num:\n")
-    b=ComRe.input_type(a)
-    print(b)
+#调用测试
+# while 1:
+#     a=input("input num:\n")
+#     b=ComRe.input_type(a)
+#     print(b)
+import pymysql
+#pymysql.install_as_MySQLdb()
+def py_mysql_get_version():
+    db = pymysql.connect("localhost","root","root","brow")
+    cursor = db.cursor()
+    sql="select * from admin"
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    print(data[0][1])
+    # for i in data:
+    #     print(i)
+
+    db.close()
+py_mysql_get_version()
